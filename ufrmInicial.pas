a107d791bebc83a3af8f93cbdc1964a019358e70
Lucas Guimarães <lucas.guimaraes@softplan.com.br>
2018-09-10 12:03:28 -0300

    Construindo layout

--- ufrmInicial.pas
+++ ufrmInicial.pas
@@ -4,10 +4,20 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects, FMX.Layouts,
+  FMX.Controls.Presentation, FMX.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    lytLogo: TLayout;
+    lytSubTituloLogo: TLayout;
+    recLogoInstagram: TRectangle;
+    lytLoginFacebookBottom: TLayout;
+    lytOu: TLayout;
+    lytCadastrarTelefoneOuEmail: TLayout;
+    lytJaTemConta: TLayout;
+    lytLoginFacebookGeral: TLayout;
+    btnLoginFacebook: TSpeedButton;
   private
     { Private declarations }
   public
