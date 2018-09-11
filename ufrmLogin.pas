5ee87650259b3b76a39e64c4c0f2864ff698ed51
Lucas Guimarães <lucas.guimaraes@softplan.com.br>
2018-09-11 14:38:27 -0300

    Construção de etapas de cadastro (Transição)

--- ufrmLogin.pas
+++ ufrmLogin.pas
@@ -5,14 +5,18 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
-  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, FMX.TabControl, System.Actions, FMX.ActnList;
 
 type
   TfrmLogin = class(TForm)
-    recTopo: TRectangle;
-    lytLogo: TLayout;
-    imgLogoInstagram: TImage;
-    lblSubTituloLogo: TLabel;
+    tbcLogin: TTabControl;
+    tabCadastro1: TTabItem;
+    tabCadastro2: TTabItem;
+    tabCadastro3: TTabItem;
+    tabLogin: TTabItem;
+    lytCadastro1: TLayout;
+    lytCadastrarTelefoneOuEmail: TLayout;
+    lblCadastrarTelefoneEmail: TLabel;
     lytLoginFacebookBottom: TLayout;
     lytLoginFacebookGeral: TLayout;
     recLogiFacebook: TRectangle;
@@ -25,16 +29,21 @@ type
     lytEsquerdaOu: TLayout;
     lineEsquerda: TLine;
     lblOu: TLabel;
-    lytCadastrarTelefoneOuEmail: TLayout;
-    lblCadastrarTelefoneEmail: TLabel;
     lytRodape: TLayout;
     lytJaTemConta: TLayout;
     recRodape: TRectangle;
     lblJaTemConta: TLabel;
     lblLinkLogin: TLabel;
     lineJaTemContaTopo: TLine;
+    recTopo: TRectangle;
+    lytLogo: TLayout;
+    imgLogoInstagram: TImage;
+    lblSubTituloLogo: TLabel;
+    lstAcoes: TActionList;
+    actMudaAba: TChangeTabAction;
+    procedure lblCadastrarTelefoneEmailClick(Sender: TObject);
   private
-    { Private declarations }
+    procedure mudaAba(pAba: TTabItem);
   public
     { Public declarations }
   end;
@@ -45,5 +54,16 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TfrmLogin.lblCadastrarTelefoneEmailClick(Sender: TObject);
+begin
+  mudaAba(tabCadastro2);
+end;
+
+procedure TfrmLogin.mudaAba(pAba: TTabItem);
+begin
+  actMudaAba.Tab := pAba;
+  actMudaAba.ExecuteTarget(nil);
+end;
 
 end.
