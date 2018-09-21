012965a05f49a0e705ebc9585849ed110521c817
Lucas Guimarães <lucas.guimaraes@softplan.com.br>
2018-09-21 08:11:17 -0300

    correção de validação

--- uClass.Validacao.pas
+++ uClass.Validacao.pas
@@ -14,12 +14,12 @@ implementation
 uses
   System.SysUtils;
 
-function emailValido(pEmail: string): Boolean;
+class function TValida.emailValido(pEmail: string): Boolean;
 begin
   Result := not pEmail.IsEmpty;
 end;
 
-function telefoneValido(pTelefone: string): Boolean;
+class function TValida.telefoneValido(pTelefone: string): Boolean;
 begin
   Result := not pTelefone.IsEmpty;
 end;
